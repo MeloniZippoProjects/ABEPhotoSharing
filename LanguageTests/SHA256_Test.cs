11fa231890b6a3e23b989bc2912a8107c60d006d
enricomeloni <enrico-meloni@hotmail.it>
2017-09-09 18:03:07 +0200

    Add test for SHA256 and self signed certificate
    Self Signed Certificate created with New-SelfSignedCertificate Cmdlet and
    exported to file with Export-Certificate cmdlet.

---
+++ LanguageTests/SHA256_Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Calcola il valore hash della matrice di byte specificata.(Ereditato da HashAlgorithm.)";
+            byte[] byteText = Encoding.UTF8.GetBytes(text);
+            MemoryStream msHash = new MemoryStream(byteText);
+
+            SHA256 sha = new SHA256Cng();
+
+            byte[] hash = sha.ComputeHash(msHash);
+
+            Console.WriteLine(BitConverter.ToString(hash).Replace("-", string.Empty));
+
+            Console.ReadLine();
+            return;
+        }
+    }
+}
