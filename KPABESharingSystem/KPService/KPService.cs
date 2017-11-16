5336048b471b038c884dfd9382650be8b3081de9
enricomeloni <enrico-meloni@hotmail.it>
2017-11-16 16:25:46 +0100

    Add KPService for shared functions

---
+++ KPABESharingSystem/KPService/KPService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPServices
+{
+    public class KPService
+    {
+
+        public static void setup()
+        {
+            return;
+        }
+
+    }
+}
