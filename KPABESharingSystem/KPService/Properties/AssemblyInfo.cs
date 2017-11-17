5a6a2995fbe2b493f936347f2c544a7a05edab48
enricomeloni <enrico-meloni@hotmail.it>
2017-11-17 02:19:22 +0100

    Change to solution

--- KPABESharingSystem/KPService/Properties/AssemblyInfo.cs
+++ KPABESharingSystem/KPService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("KPService")]
+[assembly: AssemblyTitle("KPServices")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KPService")]
+[assembly: AssemblyProduct("KPServices")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
