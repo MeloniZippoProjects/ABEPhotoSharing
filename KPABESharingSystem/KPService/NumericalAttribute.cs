16216fd51ae660b6579093a06d67dcac44384ade
enricomeloni <enrico-meloni@hotmail.it>
2017-11-17 16:00:54 +0100

    Created a Policy class and moved code from NumericalAttribute in Policy

--- KPABESharingSystem/KPService/NumericalAttribute.cs
+++ KPABESharingSystem/KPService/NumericalAttribute.cs
@@ -6,18 +6,48 @@ using System.Threading.Tasks;
 
 namespace KPServices
 {
+
+    
+
     public class NumericalAttribute : UniverseAttribute
     {
-        public int Number;
-
-        public NumericalAttribute(string name, int number) : base(name)
+        private UInt64 number = 0;
+        public UInt64 Number
         {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                if (value >= (2 ^ (UInt64)NumberResolution))
+                    throw new ArgumentOutOfRangeException("value", String.Format("The value {0} can't be stored in {1} bits", value, NumberResolution));
+                number = value;
+            }
+        }
+        public byte NumberResolution
+        {
+            get;
+            set;
+        } = 64;
+
+        public NumericalAttribute(string name) : base(name) {}
+
+        public NumericalAttribute(string name, UInt64 number) : base(name)
+        {
+            Number = number;
+        }
+
+        public NumericalAttribute(string name, UInt64 number, byte numberResolution) : base(name)
+        {
+            //the assignments must be in this order to properly check if number can stay in numberResolution bits
+            NumberResolution = numberResolution;
             Number = number;
         }
 
         public override string ToString()
         {
-            return Name + "=" + Number;
+            return Name + "=";
         }
     }
 }
