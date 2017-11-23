1f8bb359cf211b335134cf15f79d847f9d950753
rzippo <raffaele.zippo@live.it>
2017-11-23 22:26:50 +0100

    KPClient: implemented encryption and upload process for single image. BUG when using kpabe

--- KPABESharingSystem/KPClient/UploadImagesWindow.xaml.cs
+++ KPABESharingSystem/KPClient/UploadImagesWindow.xaml.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,6 +19,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using KPServices;
+using Newtonsoft.Json;
+using Path = System.IO.Path;
 
 namespace KPClient
 {
@@ -78,6 +86,90 @@ namespace KPClient
         private bool ValidateTags()
         {
             return false;
+        }
+
+        private void UploadButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (ImageItems.Count == 1)
+                UploadImage(ImageItems.First().ImagePath);
+            else
+                UploadAlbum(ImageItems.Select(item => item.ImagePath).ToArray());
+
+
+              
+        }
+
+        private void UploadImage(string imagePath)
+        {
+            try
+            {
+                string workingDir = Path.GetTempPath();
+                Console.WriteLine(workingDir);
+                string basename = Path.GetFileNameWithoutExtension(imagePath);
+                string convertedFilepath = Path.Combine(workingDir, $"{basename}.png");
+
+                Bitmap b = new Bitmap(imagePath);
+                using (FileStream fs = new FileStream(convertedFilepath, FileMode.Create)) {
+                    b.Save(fs, ImageFormat.Png);
+                }
+
+                Aes aes = new AesCng();
+                aes.KeySize = 256;
+                aes.GenerateIV();
+                aes.GenerateKey();
+
+                string keypath = Path.Combine(workingDir, $"{basename}.key");
+                var key = new
+                {
+                    IV = aes.IV,
+                    Key = aes.Key
+                };
+                using (FileStream fs = new FileStream(keypath, FileMode.Create))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        string serializedKey = JsonConvert.SerializeObject(key);
+                        sw.Write(serializedKey);
+                    }
+                }
+
+                string encryptedKeyPath = $"{keypath}.kpabe";
+
+                //todo: should be configured at initialization
+                KPService.SuitePath = "./kpabe/";
+                KPService.PublicKey = "./pub_key";
+                KPService.Encrypt(keypath, TagsSelector.GetTagsString(), false, encryptedKeyPath);
+                
+                //Debug.Assert(File.Exists(encryptedKeyPath));
+
+                var encryptor = aes.CreateEncryptor();
+
+                string encryptedImagePath = $"{convertedFilepath}.aes";
+                using (FileStream outputFS = new FileStream(encryptedImagePath, FileMode.Create))
+                {
+                    using (CryptoStream encryptCS = new CryptoStream(outputFS, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (FileStream inputFS = new FileStream(convertedFilepath, FileMode.Open))
+                        {
+                            inputFS.CopyTo(encryptCS);
+                        }
+                    }
+                }
+
+                File.Copy(encryptedKeyPath,
+                    Path.Combine(Properties.Settings.Default.SharedFolderPath, "keys", Path.GetFileName(encryptedKeyPath)));
+                File.Copy(encryptedImagePath,
+                    Path.Combine(Properties.Settings.Default.SharedFolderPath, "items", Path.GetFileName(encryptedImagePath)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Something went wrong: {ex}");
+            }
+        }
+
+        private void UploadAlbum(string[] imagePaths)
+        {
+            MessageBox.Show("Not yet implemented!");
         }
     }
 
