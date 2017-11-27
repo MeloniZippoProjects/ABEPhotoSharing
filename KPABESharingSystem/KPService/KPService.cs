33987c4969c872a2d0d0c1b70d09ba32667a4e26
rzippo <raffaele.zippo@live.it>
2017-11-27 13:35:23 +0100

    KPServices: redesigned KPService as an object representing a KPABE session

--- KPABESharingSystem/KPService/KPService.cs
+++ KPABESharingSystem/KPService/KPService.cs
@@ -2,73 +2,54 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 namespace KPServices
 {
+    public class ExeNames
+    {
+        public string Setup;
+        public string Keygen;
+        public string Encrypt;
+        public string Decrypt;
+    }
+
+    public class Keys
+    {
+        public byte[] MasterKey;
+        public byte[] PublicKey;
+        public byte[] PrivateKey;
+    }
+    
     /// <summary>
     /// Provides utilities for the KPABE suite
     /// </summary>
     public class KPService
     {
-       
-        /*
-        /// <summary>
-        /// Configurable name for universe file
-        /// </summary>
-        public static String UniversePath = "universe";
-        */
-        
         /// <summary>
         /// Configurable path for the KPABE suite
         /// </summary>
-        public static String SuitePath = @"kpabe\";
+        public static String SuitePath = null;
 
         /// <summary>
-        /// Configurable filename for KPABE Setup
+        /// Configurable filenames for KPABE tools
         /// </summary>
-        public static String SetupExe = "kpabe-setup.exe";
-
-        /// <summary>
-        /// Configurable filename for KPABE Encrypt
-        /// </summary>
-        public static String EncryptExe = "kpabe-enc.exe";
-
-        /// <summary>
-        /// Configurable filename for KPABE Decrypt
-        /// </summary>
-        public static String DecryptExe = "kpabe-dec.exe";
-
-        /// <summary>
-        /// Configurable filename for KPABE Keygen
-        /// </summary>
-        public static String KeygenExe = "kpabe-keygen.exe";
-
-        /// <summary>
-        /// Configurable filename for KPABE master key
-        /// </summary>
-        public static String MasterKey = "master_key";
-
-        /// <summary>
-        /// Configurable filename for KPABE public key
-        /// </summary>
-        public static String PublicKeyPath = "pub_key";
-
-        private static Universe universe;
-
-        public static Universe Universe
+        public static ExeNames ExeNames = new ExeNames()
         {
-            get
-            {
-                if (universe == null)
-                    throw new UniverseNotDefinedException($"Universe is not defined");
-                else
-                    return universe;
-            }
-            set => universe = value;
+            Setup = "kpabe-setup.exe",
+            Keygen = "kpabe-keygen.exe",
+            Encrypt = "kpabe-enc.exe",
+            Decrypt = "kpabe-dec.exe"
+        };
+
+        private static string GetTool(string toolName)
+        {
+            String path = Path.Combine(SuitePath ?? Directory.GetCurrentDirectory(), toolName);
+            return path;
         }
 
-        private static void PrepareStartInfo(ProcessStartInfo startInfo)
+        private static void PrepareProcessStart(ProcessStartInfo startInfo)
         {
             startInfo.UseShellExecute = false;
             startInfo.RedirectStandardOutput = true;
@@ -76,19 +57,30 @@ namespace KPServices
         }
 
         /// <summary>
+        /// Configurable filenames for KPABE keys
+        /// </summary>
+        public Keys Keys = new Keys();
+      
+        public static Universe Universe { get; set; }
+        
+        /// <summary>
         /// Setups the KPABE encryption suite by creating the 
         /// master key and the public key.
         /// 
         /// It executes the file kpabe-setup.exe with 
         /// the current universe as argument.
         /// </summary>
-        public static void Setup()
+        public void Setup()
         {
-            String kpabeSetupPath = SuitePath + SetupExe;
+            String setupPath = GetTool(ExeNames.Setup);
 
+            string publicKey = Path.GetTempFileName();
+            string masterKey = Path.GetTempFileName();
+              
             Process kpabeSetupProcess = new Process();
-            kpabeSetupProcess.StartInfo.FileName = kpabeSetupPath;
-            PrepareStartInfo(kpabeSetupProcess.StartInfo);
+            kpabeSetupProcess.StartInfo.FileName = setupPath;
+            kpabeSetupProcess.StartInfo.Arguments = $"-p \"{publicKey}\" -m \"{masterKey}\" {Universe}";
+            PrepareProcessStart(kpabeSetupProcess.StartInfo);
 
             try
             {
@@ -96,46 +88,35 @@ namespace KPServices
             }
             catch(System.ComponentModel.Win32Exception)
             {
-                throw new SuiteExeNotFound($"Cannot find {SuitePath}{SetupExe}");
+                throw new SuiteExeNotFound($"Cannot find {setupPath}");
             }
 
             String stderr = kpabeSetupProcess.StandardOutput.ReadToEnd();
             kpabeSetupProcess.WaitForExit();
 
-
             if (!stderr.Equals("") || kpabeSetupProcess.ExitCode != 1)
                 throw new SuiteErrorException("Error during KPABE Setup");
-            
-            //todo: any checks for the result?? Errors?
+
+            Keys.PublicKey = File.ReadAllBytes(publicKey);
+            Keys.MasterKey = File.ReadAllBytes(masterKey);
         }
 
-        /*
-        public static void Keygen(IEnumerable<PolicyElement> policies, String outputFile = "")
+        public void Keygen(String policy)
         {
-            String policyString = "";
-            foreach(PolicyElement policy in policies)
-            {
-                policyString += policy + " ";
-            }
+            String keygenPath = GetTool(ExeNames.Keygen);
 
-            policyString = policyString.Substring(0, policyString.Length - 1);
-        }
-        */
+            string privateKey = Path.GetTempFileName();
+            string publicKey = Path.GetTempFileName();
+            string masterKey = Path.GetTempFileName();
 
-        //todo: check policy format, should add quotes here or in caller?
-        public static void Keygen(String policy, String outputFile = "")
-        {
-            String kpabeKeygenPath = SuitePath + KeygenExe;
+            File.WriteAllBytes(publicKey, Keys.PublicKey);
+            File.WriteAllBytes(masterKey, Keys.MasterKey);
 
             Process kpabeKeygenProcess = new Process();
-            //String pwd = Directory.GetCurrentDirectory();
-            kpabeKeygenProcess.StartInfo.FileName = kpabeKeygenPath;
-            PrepareStartInfo(kpabeKeygenProcess.StartInfo);
+            kpabeKeygenProcess.StartInfo.FileName = keygenPath;
+            PrepareProcessStart(kpabeKeygenProcess.StartInfo);
 
-            //create argument string and specify output if outputFile is not empty
-            String argumentsString = String.IsNullOrEmpty(outputFile) ? "" : $" --output {outputFile}";
-            argumentsString += $" \"{PublicKeyPath}\" \"{MasterKey}\" {policy}";
-            //Console.WriteLine(argumentsString);
+            String argumentsString = $" --output {privateKey} \"{publicKey}\" \"{masterKey}\" {policy} ";
             kpabeKeygenProcess.StartInfo.Arguments = argumentsString;
 
             try
@@ -144,7 +125,7 @@ namespace KPServices
             }
             catch (System.ComponentModel.Win32Exception)
             {
-                throw new SuiteExeNotFound($"Cannot find {SuitePath}{SetupExe}");
+                throw new SuiteExeNotFound($"Cannot find {keygenPath}");
             }
 
             String stderr = kpabeKeygenProcess.StandardError.ReadToEnd();
@@ -162,58 +143,50 @@ namespace KPServices
             if (!stderr.Equals("") || kpabeKeygenProcess.ExitCode != 0)
                 throw new SuiteErrorException("Error during KPABE Setup");
 
-            Console.WriteLine(stderr);
-            //todo: any checks for the result?? Errors?
+            Keys.PrivateKey = File.ReadAllBytes(privateKey);
         }
 
-        public static void Encrypt(String sourceFilePath, String attributes, bool deleteSourceFile = false, String outputFile = "")
+        public void Encrypt(String sourceFilePath, String destFilePath, String attributes, bool deleteSourceFile = false )
         {
-            String kpabeEncryptPath = Path.Combine(SuitePath, EncryptExe);
-            Console.WriteLine(kpabeEncryptPath);
+            String encryptPath = GetTool(ExeNames.Encrypt);
 
-            Process kpabeEncryptProcess = new Process();
-            kpabeEncryptProcess.StartInfo.FileName = kpabeEncryptPath;
-            //kpabeEncryptProcess.StartInfo.CreateNoWindow = true;
-            kpabeEncryptProcess.StartInfo.UseShellExecute = false;
-            kpabeEncryptProcess.StartInfo.RedirectStandardOutput = true;
+            String publicKey = Path.GetTempFileName();
+            File.WriteAllBytes(publicKey, Keys.PublicKey);
 
-            String argumentString = ( deleteSourceFile ? "" : " --keep-input-file" ) +  (String.IsNullOrEmpty(outputFile) ? "" : $" --output \"{outputFile}\"" );
-            argumentString += $" \"{PublicKeyPath}\" \"{sourceFilePath}\" {attributes}";
-            Console.WriteLine(argumentString);
-            kpabeEncryptProcess.StartInfo.Arguments = argumentString;
+            Process encryptProcess = new Process();
+            encryptProcess.StartInfo.FileName = encryptPath;
+            encryptProcess.StartInfo.Arguments = $"{(deleteSourceFile ? "" : "--keep - input - file")} --output \"{destFilePath}\" \"{publicKey}\" \"{sourceFilePath}\" {attributes}";
+            PrepareProcessStart(encryptProcess.StartInfo);
 
-            kpabeEncryptProcess.Start();
-            String stdout = kpabeEncryptProcess.StandardOutput.ReadToEnd();
-            kpabeEncryptProcess.WaitForExit();
+            encryptProcess.Start();
 
-            Console.WriteLine(stdout);
-
-            //todo: any checks for the result?? Errors?
+            //todo: any checks for errors?
+            
+            String stdout = encryptProcess.StandardError.ReadToEnd();
+            encryptProcess.WaitForExit();
         }
 
-        public static void Decrypt(String sourceFilePath, String privateKeyFilePath, bool deleteSourceFile = false, String outputFile = "")
+        public void Decrypt(String sourceFilePath, String destFilePath, bool deleteSourceFile = false)
         {
-            String kpabeDecryptPath = Path.Combine(SuitePath, DecryptExe);
-            Console.WriteLine(kpabeDecryptPath);
+            String decryptPath = GetTool(ExeNames.Decrypt);
 
-            Process kpabeDecryptProcess = new Process();
-            kpabeDecryptProcess.StartInfo.FileName = kpabeDecryptPath;
-            //kpabeDecryptProcess.StartInfo.CreateNoWindow = true;
-            kpabeDecryptProcess.StartInfo.UseShellExecute = false;
-            kpabeDecryptProcess.StartInfo.RedirectStandardOutput = true;
+            String publicKey = Path.GetTempFileName();
+            File.WriteAllBytes(publicKey, Keys.PublicKey);
 
-            String argumentString = (deleteSourceFile ? "" : " --keep-input-file") + (String.IsNullOrEmpty(outputFile) ? "" : $" --output \"{outputFile}\"");
-            argumentString += $" \"{PublicKeyPath}\" \"{privateKeyFilePath}\" \"{sourceFilePath}\"";
-            Console.WriteLine(argumentString);
-            kpabeDecryptProcess.StartInfo.Arguments = argumentString;
+            String privateKey = Path.GetTempFileName();
+            File.WriteAllBytes(privateKey, Keys.PrivateKey);
 
-            kpabeDecryptProcess.Start();
-            String stdout = kpabeDecryptProcess.StandardOutput.ReadToEnd();
-            kpabeDecryptProcess.WaitForExit();
+            Process decryptProcess = new Process();
+            decryptProcess.StartInfo.FileName = decryptPath;
+            decryptProcess.StartInfo.Arguments = $"{(deleteSourceFile ? "" : " --keep-input-file")} --output \"{destFilePath}\" \"{publicKey}\" \"{privateKey}\" \"{sourceFilePath}\"";
+            PrepareProcessStart(decryptProcess.StartInfo);
 
-            Console.WriteLine(stdout);
+            decryptProcess.Start();
 
-            //todo: any checks for the result?? Errors?
+            //todo: any checks for errors?
+
+            String stdout = decryptProcess.StandardError.ReadToEnd();
+            decryptProcess.WaitForExit();
         }
     }
 }
