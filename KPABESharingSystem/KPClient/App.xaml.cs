d41c154358ea50aa8b57e61f0a93a09c92d2897b
enricomeloni <enrico-meloni@hotmail.it>
2017-11-27 18:06:49 +0100

    Add login form

--- KPABESharingSystem/KPClient/App.xaml.cs
+++ KPABESharingSystem/KPClient/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
+using Grapevine.Client;
 using KPServices;
 
 namespace KPClient
@@ -15,9 +17,36 @@ namespace KPClient
     public partial class App : Application
     {
         public Universe Universe;
+        public string Username;
+        public string Password;
+        public RestClient RestClient;
 
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
+
+            if (String.IsNullOrEmpty(KPClient.Properties.Settings.Default.ServerAddress))
+            {
+                KPClient.Properties.Settings.Default.ServerAddress = @"localhost";
+                KPClient.Properties.Settings.Default.Save();
+            }
+
+            if (KPClient.Properties.Settings.Default.ServerPort == 0)
+            {
+                KPClient.Properties.Settings.Default.ServerPort = 1234;
+                KPClient.Properties.Settings.Default.Save();
+            }
+            RestClient = new RestClient
+            {
+                Host = KPClient.Properties.Settings.Default.ServerAddress,
+                Port = KPClient.Properties.Settings.Default.ServerPort
+            };
+
+            if (Username == null || Password == null)
+            {
+                LoginForm loginForm = new LoginForm();
+                loginForm.Show();
+            }
+
             if (String.IsNullOrEmpty(KPClient.Properties.Settings.Default.Universe))
             {
                 //todo: should ask server
@@ -29,6 +58,7 @@ namespace KPClient
 #if DEBUG
             System.Windows.MessageBox.Show($"Universe is: {((App)Application.Current).Universe}");
 #endif
+
 
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
---
+++ KPABESharingSystem/KPClient/LoginForm.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Grapevine.Client;
+using Grapevine.Shared;
+
+namespace KPClient
+{
+    /// <summary>
+    /// Logica di interazione per LoginForm.xaml
+    /// </summary>
+    public partial class LoginForm : Window
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var username = UsernameTextBox.Text;
+            var password = PasswordTextBox.Password;
+
+            RestRequest request = new RestRequest
+            {
+                Payload = $"username={username};password={password}",
+                Encoding = Encoding.UTF8,
+                HttpMethod = HttpMethod.POST,
+                ContentType = ContentType.TXT,
+                RequestUri = new Uri("/login")
+            };
+            var response = ((App) Application.Current).RestClient.Execute(request);
+            if (response.StatusCode == HttpStatusCode.Ok)
+                this.Close();
+            else
+                ErrorLabel.Content = "Wrong Username or Password";
+        }
+    }
+}
