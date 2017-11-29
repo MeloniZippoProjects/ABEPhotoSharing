f2258beb49b33f275d83da7edadc6c299570b5c7
enricomeloni <enrico-meloni@hotmail.it>
2017-11-29 12:46:03 +0100

    KPService: fix KPExceptions not recognized by visual studio

--- KPABESharingSystem/KPService/KPExceptions.cs
+++ KPABESharingSystem/KPService/KPExceptions.cs
@@ -8,44 +8,44 @@ namespace KPServices
 {
     public class SuiteException : Exception
     {
-        public SuiteException() {}
-        public SuiteException(String message) : base(message){}
+        public SuiteException() { }
+        public SuiteException(String message) : base(message) { }
     }
 
     public class ToolNotFound : SuiteException
     {
-        public ToolNotFound(){}
-        public ToolNotFound(String message) : base(message){}
+        public ToolNotFound() { }
+        public ToolNotFound(String message) : base(message) { }
     }
 
     public class SetupException : SuiteException
     {
         public SetupException() { }
-        public SetupException(String message) : base(message){ }
+        public SetupException(String message) : base(message) { }
     }
 
     public class UniverseNotDefined : SuiteException
     {
         public UniverseNotDefined() { }
-        public UniverseNotDefined(String message) : base(message){ }
+        public UniverseNotDefined(String message) : base(message) { }
     }
 
     public class KeygenException : SuiteException
     {
         public KeygenException() { }
-        public KeygenException(String message) : base(message){ }
+        public KeygenException(String message) : base(message) { }
     }
 
     public class TrivialPolicy : KeygenException
     {
-        public TrivialPolicy(){}
-        public TrivialPolicy(string message) : base(message){}
+        public TrivialPolicy() { }
+        public TrivialPolicy(string message) : base(message) { }
     }
 
     public class UnsatisfiablePolicy : KeygenException
     {
-        public UnsatisfiablePolicy(){}
-        public UnsatisfiablePolicy(string message) : base(message){}
+        public UnsatisfiablePolicy() { }
+        public UnsatisfiablePolicy(string message) : base(message) { }
     }
 
     public class AttributeNotFound : KeygenException
@@ -57,7 +57,7 @@ namespace KPServices
     public class EncryptException : SuiteException
     {
         public EncryptException() { }
-        public EncryptException(String message) : base(message){ }
+        public EncryptException(String message) : base(message) { }
     }
 
     public class DecryptException : SuiteException
