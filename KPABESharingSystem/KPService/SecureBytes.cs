5483ca9008b6e8d800c6975f0b1e18c23c67da65
rzippo <raffaele.zippo@live.it>
2018-01-10 19:14:04 +0100

    KPService: added padding management in SecureBytes, to protect arrays of any length

--- KPABESharingSystem/KPService/SecureBytes.cs
+++ KPABESharingSystem/KPService/SecureBytes.cs
@@ -7,16 +7,27 @@ public class SecureBytes
     private byte[] protectedBytes;
     public byte[] ProtectedBytes
     {
-        get{
-            byte[] unprotectedBytes = (byte[]) protectedBytes.Clone();
-            ProtectedMemory.Unprotect( unprotectedBytes, MemoryProtectionScope.SameProcess);
-            return unprotectedBytes;
+        get
+        {
+            using (TemporaryBytes tb = (byte[]) protectedBytes.Clone())
+            {
+                ProtectedMemory.Unprotect(tb.Bytes, MemoryProtectionScope.SameProcess);
+                int paddingLength = tb.Bytes[tb.Bytes.Length - 1];
+                int contentLength = tb.Bytes.Length - paddingLength;
+                byte[] unprotectedBytes = new byte[contentLength];
+                Array.Copy(sourceArray: tb, destinationArray:unprotectedBytes, length: contentLength);
+                return unprotectedBytes;
+            }
         }
-        set{
-            protectedBytes = (byte[]) value.Clone();
-            ProtectedMemory.Protect( protectedBytes, MemoryProtectionScope.SameProcess);
+        set
+        {
+            int paddedLength =  ((int) Math.Ceiling((decimal) (value.Length + 1) / 16)) * 16;
+            protectedBytes = new byte[paddedLength];
+            Array.Copy(sourceArray: value, destinationArray: protectedBytes, length: value.Length);
+            for (int idx = value.Length; idx < protectedBytes.Length; idx++)
+                protectedBytes[idx] = (byte) (paddedLength - value.Length);
+            ProtectedMemory.Protect(protectedBytes, MemoryProtectionScope.SameProcess);
         }
-
     }
 
     public static implicit operator TemporaryBytes (SecureBytes secureBytes)
